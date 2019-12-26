f466c1cbfc3518985c10517dcfe0537fd87943ad
Uruncle <bobdobs@users.noreply.github.com>
2019-12-26 12:06:37 +0100

    Slightly better output and resetting timer when there is no block (eg. when Jormungander is bootstrapping)

--- stuck_node_mon.sh
+++ stuck_node_mon.sh
@@ -12,34 +12,33 @@
 ### CONFIGURATION
 export PATH=$PATH:/home/uruncle/.cargo/bin
 JCLI="jcli"
-JCLI_PORT=3100
+JCLI_PORT=8443
 LAST_BLOCK=""
-RESTART_GT=180
-
 START_TIME=$SECONDS
+RESTART_GT=360
 
 while true
 do
     TIME=$(date '+%Y-%m-%d %H:%M:%S')
-    echo ""
-    echo "${TIME} - Press [CTRL+C] to stop..."
     LATEST_BLOCK=$($JCLI rest v0 node stats get --host "http://127.0.0.1:${JCLI_PORT}/api" | grep lastBlockHeight | awk '{print $2}')
 
     if [ "$LATEST_BLOCK" > 0 ]; then
         if [ "$LATEST_BLOCK" != "$LAST_BLOCK" ]; then
             START_TIME=$(($SECONDS))
-            echo "New block height: ${LATEST_BLOCK}"
+            echo "${TIME} New block height: ${LATEST_BLOCK}"
             LAST_BLOCK="$LATEST_BLOCK"
         else
             ELAPSED_TIME=$(($SECONDS - $START_TIME))
-            echo "Current block height: ${LATEST_BLOCK} - No new block for ${ELAPSED_TIME} sec."
+            echo "${TIME} Current block height: ${LATEST_BLOCK} - No new block for ${ELAPSED_TIME} sec."
             if [ "$ELAPSED_TIME" -gt "$RESTART_GT" ]; then
-                echo "Restarting jormungandr"
+                echo ""
+                echo "${TIME} Restarting jormungandr"
                 jcli rest v0 shutdown get --host "http://127.0.0.1:${JCLI_PORT}/api"
                 echo "Sleeping for 5 sec."
                 sleep 5
                 echo "Sending restart command to screen"
                 screen -S ada -X stuff "./jmg_start_pool.sh^M"
+                #screen -S ada -X stuff "./jmg_start_node.sh^M"
                 LAST_BLOCK="$LATEST_BLOCK"
                 echo "Sleeping for 60 sec."
                 sleep 60
@@ -47,8 +46,10 @@ do
         fi
     else
         echo "No block height"
+        # Reset time
+        START_TIME=$(($SECONDS))
     fi
-    sleep 10
+    sleep 20
 done
 
 exit 0
